64a1fb7cf4cbeb30e93656877e8d434d686761e4
Khoa18062005 <nguyenquockhoa00725005@gmail.com>
2026-03-21 21:58:07 +0700

    first commit

---
+++ src/main/java/org/example/project_cuoiky_congnghephanmem_oose/ProjectCuoiKyCongNghePhanMemOoseApplication.java
@@ -0,0 +1,13 @@
+package org.example.project_cuoiky_congnghephanmem_oose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCuoiKyCongNghePhanMemOoseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCuoiKyCongNghePhanMemOoseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/project_cuoiky_congnghephanmem_oose/ProjectCuoiKyCongNghePhanMemOoseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project_cuoiky_congnghephanmem_oose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCuoiKyCongNghePhanMemOoseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
