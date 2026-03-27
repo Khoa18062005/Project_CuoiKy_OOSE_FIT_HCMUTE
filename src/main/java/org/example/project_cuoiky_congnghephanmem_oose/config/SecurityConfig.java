3ac8e3b2691f3460cebd5ae670bd5f1b12996f5c
qu2nishere <nghiemquan200@gmail.com>
2026-03-27 10:06:52 +0700

    add jwt for login

--- src/main/java/org/example/project_cuoiky_congnghephanmem_oose/config/SecurityConfig.java
+++ src/main/java/org/example/project_cuoiky_congnghephanmem_oose/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.project_cuoiky_congnghephanmem_oose.config;
 
+import org.example.project_cuoiky_congnghephanmem_oose.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,10 +10,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter; // ← thêm
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) { // ← thêm constructor
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -43,7 +51,7 @@ public class SecurityConfig {
                         ).hasRole("CUSTOMER")
 
                         .anyRequest().authenticated()
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
